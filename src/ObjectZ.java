4f001be8dd380eefaad549a4f108325cdbb85e13
Mirek <miroslaw.kaczmarek@opoczta.pl>
2022-01-23 22:47:12 +0100

    rebase from master

--- src/ObjectZ.java
+++ src/ObjectZ.java
@@ -2,5 +2,10 @@ import java.util.Date;
 
 public class ObjectZ {
     private long p;
+    private int a1;
+    private int a2;
+    private int a3;
+    private int a4;
+    private long w1;
     Date d1;
 }
